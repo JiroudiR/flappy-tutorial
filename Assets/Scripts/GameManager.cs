132c1755839d066afb6355b768929effdac3df0c
JiroudiR <112195052+JiroudiR@users.noreply.github.com>
2023-10-09 14:35:25 -0700

    Desktop 10/9
    Worked from 52:24 to the end.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+	public Player player;
+	public Text scoreText;
+	public GameObject playButton;
+	public GameObject gameOver;
+	public GameObject getReady;
     private int score;
-	
-	public void GameOver()
+
+    private void Awake()
+    {
+		Application.targetFrameRate = 60;
+		Pause();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+    }
+
+    public void Play()
 	{
-		Debug.Log("Game over");
+		score = 0;
+		scoreText.text = score.ToString();
+		playButton.SetActive(false);
+		gameOver.SetActive(false);
+		getReady.SetActive(false);
+		Time.timeScale = 1f;
+		player.enabled = true;
+		Pipes[] pipes = FindObjectsOfType<Pipes>();
+		for (int i = 0; i < pipes.Length; i++) {
+			Destroy(pipes[i].gameObject);
+		}
+	}
+
+	public void Pause()
+	{
+		Time.timeScale = 0f;
+		player.enabled = false;
+	}
+
+    public void GameOver()
+	{
+		gameOver.SetActive(true);
+		getReady.SetActive(false);
+		playButton.SetActive(true);
+		Pause();
 	}
 	
 	public void IncreaseScore()
 	{
 		score++;
+		scoreText.text = score.ToString();
 	}
 }
