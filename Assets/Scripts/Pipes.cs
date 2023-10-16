50945b53692a1e7a3259b64bdf12866b905313e9
JiroudiR <112195052+JiroudiR@users.noreply.github.com>
2023-10-15 19:34:56 -0700

    Laptop 10/15
    Added new lava sprites and changed the Pipes script to render multiple sprites in a cycle. However, I still haven't figured out how to make it cycle inside of the pipes in while being transformed. Removed default unused sprites.

--- Assets/Scripts/Pipes.cs
+++ Assets/Scripts/Pipes.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class Pipes : MonoBehaviour
 {
     public float speed = 5f;
+	private SpriteRenderer spriteRenderer;
+	public Sprite[] sprites;
+	private int spriteIndex;
     private float leftEdge;
+	
+	private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Start()
     {
         leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+		InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
     }
 
     private void Update()
@@ -18,5 +27,16 @@ public class Pipes : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+	
+	private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if(spriteIndex >= sprites.Length) {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
